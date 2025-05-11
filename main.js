3b919af63af76f9d1440d3695fefc0e49fd3a87f
Kritika-2009 <kritika.41535@gmail.com>
2025-05-11 12:06:29 +0530

    index changed

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 console.log("asdf asdf adfs ");
 
-document.getElementsByClassName("krit")[0].style.backgroundColor = "khaki";
+document.getElementsByClassName("krit")[1].style.backgroundColor = "khaki";
