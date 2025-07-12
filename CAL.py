7cb149989c53a63d34d06a05ae61b3d132c82f26
Kritika-2009 <kritika.41535@gmail.com>
2025-07-12 19:46:27 +0530

    1

---
+++ CAL.py
@@ -0,0 +1,68 @@
+import re
+
+lis = []
+arr = []
+test_str = input ()
+
+
+regex = r"\d+"
+matches = re.finditer(regex, test_str)
+for matchNum, match in enumerate(matches, start=1):
+    
+	dig = "Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group())
+	initial = dig.split()
+	lis.append(initial[6])
+#print (lis)
+
+
+regex = r"\W+"
+matches = re.finditer(regex, test_str)
+for matchNum, match in enumerate(matches, start=1):   
+
+	ops = "Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group())
+	intro = ops.split()
+	arr.append(intro[6])
+#print (arr)
+
+
+
+
+
+for op in arr:
+	match op:
+		case "+":
+			plus = arr.index(op)
+			add = int(lis[plus]) + int(lis[plus+1])
+		case "-":
+			minus = arr.index(op)
+			sub = add - int(lis[minus+1])
+		case "*":
+			mult = arr.index(op)
+			prod = sub * int(lis[mult+1])
+		case "/":
+			div = arr.index(op)
+			print(prod / int(lis[div+1]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##			
+##			
+##				print (digi*digi)
+##			
+##				print (digi/digi)
+			
+
+
+
